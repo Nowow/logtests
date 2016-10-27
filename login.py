c6ea7756c3722415554f51d463dcf6bc5aee480d
Nowow <animexones@gmail.com>
2016-10-27 06:44:43 +0300

    commit

--- login.py
+++ login.py
@@ -4,6 +4,13 @@ from simplepam import authenticate
 DEFAULT_PORT = 5000
 ADDITIVE_FOR_UID = 1000
 
+try:
+    from os import getuid
+
+except ImportError:
+    def getuid():
+        return DEFAULT_PORT - ADDITIVE_FOR_UID
+
 
 app = Flask(__name__)
 
